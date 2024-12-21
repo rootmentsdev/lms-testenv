afbaa88dcf42b04bccca2a8573b3067c08e90370
Jishnu M <mjihnu990@gmail.com>
2024-12-21 10:19:52 +0530

    description

--- backend/model/Module.js
+++ backend/model/Module.js
@@ -4,14 +4,13 @@ const questionSchema = new mongoose.Schema({
     questionText: { type: String, required: true }, // The question text
     options: [{ type: String, required: true }], // Array of multiple-choice options
     correctAnswer: { type: String, required: true }, // Correct answer text
-  
+
 
 });
 
 // Define schema for each video
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true }, // Title of the video
-    description: { type: String, required: true }, // Description of the video
     videoUri: { type: String, required: true }, // Video link or URI
     questions: [questionSchema],
     // Array of questions related to the video
@@ -20,6 +19,7 @@ const videoSchema = new mongoose.Schema({
 // Define the module schema that includes multiple videos
 const moduleSchema = new mongoose.Schema({
     moduleName: { type: String, required: true }, // Name of the module
+    description: { type: String, required: true },
     videos: [videoSchema], // Array of video objects using videoSchema
     createdAt: { type: Date, default: Date.now }, // Timestamp when the module was created
 });
