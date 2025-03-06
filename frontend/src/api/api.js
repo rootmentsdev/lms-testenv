5710767d05df6f3274d92e62a9bd1d13c2ffc2ef
Jishnu M <mjihnu990@gmail.com>
2025-03-06 16:03:59 +0530

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,6 +2,6 @@
 
 
 const baseUrl = {
-    baseUrl:'https://lms-test-mfec.onrender.com/'// 'http://3.110.163.189:7000/' //'https://lms-ildn.onrender.com/' http://localhost:7000/ https://lms-test-mfec.onrender.com
+    baseUrl:'https://lms-1-lavs.onrender.com/'// https://lms-1-lavs.onrender.com 'http://3.110.163.189:7000/' //'https://lms-ildn.onrender.com/' http://localhost:7000/ https://lms-test-mfec.onrender.com
 }
 export default baseUrl
