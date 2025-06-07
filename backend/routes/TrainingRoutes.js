993ebcd3a5bd8ce433152f6abd350a96e75b7cf9
rootmentsdev <rootmentsit@gmail.com>
2025-06-07 11:37:48 +0530

    migration

--- backend/routes/TrainingRoutes.js
+++ backend/routes/TrainingRoutes.js
@@ -1,13 +1,13 @@
 
 import express from 'express';
 import { migrateFoundationOfServiceTraining } from '../controllers/TrainingController.js';
-import { MiddilWare } from '../lib/middilWare.js';   // remove if no auth
+// import { MiddilWare } from '../lib/middilWare.js';   // remove if no auth
 
 const router = express.Router();
 
 router.post(
   '/migrate/foundationTraining',
-  MiddilWare,                         // comment out if not needed
+//   MiddilWare,                         // comment out if not needed
   migrateFoundationOfServiceTraining
 );
 
