54f4f4a148cb23447bb72e0cd7f8bef161ef8e69
rootmentsit <rootmentsit@gmail.com>
2025-08-08 10:29:18 +0530

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,7 +5,7 @@ const baseUrl = {
     baseUrl: "https://lms-testenv.onrender.com/"
 
 
-//this code is in test environment   https://lms-testenv.onrender.com/
+//this code is in test environment   https://lms-testenv.onrender.com/ 
 }; 
 
 export default baseUrl;
