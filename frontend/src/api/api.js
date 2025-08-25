66defa4c082244bea5f2180c3026befa623f969e
rootmentsit <rootmentsit@gmail.com>
2025-08-25 14:57:47 +0530

    Api added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,10 +2,10 @@
 const baseUrl = {
 
     
-    baseUrl: "http://localhost:7000/"
+    baseUrl: "https://lms-testenv.onrender.com/"
 
 
-//this code is in test environment   https://lms-testenv.onrender.com/ 
+//this code is in test environment   https://lms-testenv.onrender.com/   http://localhost:7000/
 }; 
 
 export default baseUrl;
